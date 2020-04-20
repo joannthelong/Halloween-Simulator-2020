a7dd41b647a584dc7423ec972a6f513ea42ad70c
joannthelong <jlong4@ucsc.edu>
2020-04-19 17:59:19 -0700

    now with point breakdown

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+/*
+Joann Long
+CMPM120
+4/19/2020
+
+Points breakdown:
+  Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (50)
+  Implement parallax scrolling (15)
+  Create a new animated sprite for the Spaceship enemies (15)
+  Allow the player to control the Rocket after it's fired (10)
+  Randomize each spaceship's movement direction at the start of each play (10)
+  Add your own (copyright-free) background music to the Play scene (10)
+
+
+  I'm not sure if some of these overlap with the 50 Point Redesign; I listed them just in case.
+  And to make a small case if necessary, parallax scrolling wasn't included in the base game, 
+  and neither was animated sprites for the Spaceship enemies (which I changed to cute ghosts).
+
+*/
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
